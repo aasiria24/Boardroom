4b6effedaabf0219bb86e83a2a9447e369302f16
aasiria24 <amaalmoasiri@gmail.com>
2025-01-20 10:12:21 +0300

    Initial Commit

---
+++ Boardroom/BoardroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomApp.swift
+//  Boardroom
+//
+//  Created by Amaal Asiri on 20/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BoardroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardroomTests/BoardroomTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomTests.swift
+//  BoardroomTests
+//
+//  Created by Amaal Asiri on 20/07/1446 AH.
+//
+
+import Testing
+@testable import Boardroom
+
+struct BoardroomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
